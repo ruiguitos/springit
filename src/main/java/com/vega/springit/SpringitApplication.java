fb5331858e0566687c6e1cfc6426adaaaacf5e18
ruiguitos <ruiguitos@hotmail.com>
2022-10-07 14:51:15 +0100

    added three classes

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,13 +1,29 @@
 package com.vega.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringitApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
+	}
+
+	CommandLineRunner runner() {
+		return args -> {
+			log.error("CommandLineRunner.run();");
+			log.warn("CommandLineRunner.run();");
+			log.info("CommandLineRunner.run();");
+			log.debug("CommandLineRunner.run();");
+			log.error("CommandLineRunner.run();");
+		};
 	}
 
 }
