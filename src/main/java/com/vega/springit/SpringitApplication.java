71363b21882a73639d325ed7f226bd2bb46d59fa
ruiguitos <ruiguitos@hotmail.com>
2022-10-08 19:32:20 +0100

    Created our entities and refactored using Project Lombok

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,13 +1,20 @@
 package com.vega.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringitApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitApplication.class, args);
+    }
+
 
 }
---
+++ src/main/java/com/vega/springit/domain/Link.java
@@ -0,0 +1,25 @@
+package com.vega.springit.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Link {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+
+    // comments
+
+
+}
