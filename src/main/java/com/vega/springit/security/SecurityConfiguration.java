1959f3797c792e229fa04cbd2d442e13d8035163
ruiguitos <ruiguitos@hotmail.com>
2022-10-10 14:19:11 +0100

    Console security configuration

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -18,19 +18,36 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
     }
 
-    @Override
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                    .requestMatchers(EndpointRequest.to("info")).permitAll()
+//                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+//                //if user is user == access is forbidden
+//                //if user is admin == access is allowed
+//                    .antMatchers("/actuator/").hasRole("ADMIN")
+//                    .antMatchers("/").permitAll()
+//                    .antMatchers("/link/submit").hasRole("USER")
+//                    .and()
+//                .formLogin();
+//    }
+
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .requestMatchers(EndpointRequest.to("info")).permitAll()
-                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
-                //if user is user == access is forbidden
-                //if user is admin == access is allowed
-                    .antMatchers("/actuator/").hasRole("ADMIN")
-                    .antMatchers("/").permitAll()
+                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                    .antMatchers("/actuator/").hasRole("ACTUATOR")
                     .antMatchers("/link/submit").hasRole("USER")
-                    .and()
-                .formLogin();
+                    .antMatchers("/link/**").permitAll()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/h2-console/**").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                    .csrf().disable()
+                    .headers().frameOptions().disable();
     }
 
     @Override
