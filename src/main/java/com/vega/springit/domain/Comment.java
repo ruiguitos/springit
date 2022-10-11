c827948f6cf0d412a0c7de74305d86d4db354c0f
ruiguitos <ruiguitos@hotmail.com>
2022-10-11 10:32:14 +0100

    We are now able to see the comments listed in our link page

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,11 +1,16 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @Getter @Setter
@@ -28,6 +33,15 @@ public class Comment extends Auditable {
     public Comment(String body, Link link) {
         this.body = body;
         this.link = link;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 
 
