bd469093ba077bb72bdc6a39bb86872d5859486c
ruiguitos <ruiguitos@hotmail.com>
2022-10-11 13:32:00 +0100

    WOrking through the transactional demo

--- src/main/java/com/vega/springit/service/UserService.java
+++ src/main/java/com/vega/springit/service/UserService.java
@@ -5,8 +5,12 @@ import com.vega.springit.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
+//@Transactional
 public class UserService {
 
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -16,12 +20,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User register(User user){
+    public User register(User user) {
         return user;
     }
 
-    public User save(User user){
-       return userRepository.save(user);
+    public User save(User user) {
+        return userRepository.save(user);
     }
 
+    //@Service
+    @Transactional
+    public void saveUsers(User... users) {
+        //begin transaction
+        for (User user : users) {
+            logger.info("Saving User: " + user.getEmail());
+            userRepository.save(user);
+        }
+    }
+//
+//    @Transactional(readOnly = true)
+//    public Optional<User> findbyId(Long id){
+//        return userRepository.findById(id);
+//    }
+//
 }
